ccebb14e9ca462f8dd777674542447c0699d9855
Victor Barrios <victor.barrios@bestsecret.com>
2021-11-14 14:05:41 +0100

    Initial Commit

---
+++ GroceryTracker/GroceryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryTrackerApp.swift
+//  GroceryTracker
+//
+//  Created by Barrios, Victor on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GroceryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
