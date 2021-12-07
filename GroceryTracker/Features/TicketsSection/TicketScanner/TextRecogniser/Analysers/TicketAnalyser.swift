b5ef25145463824d81b46b4e83874c22f6a593a5
Victor Barrios <victor.barrios@bestsecret.com>
2021-12-07 23:21:18 +0100

    Create algorithm for analysing tickets
    - Sort observations previously
    - Filter out lowercase
    - Accept 'kg' in lowercase
    - Accept currency symbols, whitespaces, punctuation and numbers

--- GroceryTracker/Features/TicketsSection/TicketScanner/TextRecogniser/Analysers/TicketAnalyser.swift
+++ GroceryTracker/Features/TicketsSection/TicketScanner/TextRecogniser/Analysers/TicketAnalyser.swift
@@ -3,9 +3,39 @@ import VisionKit
 import Vision
 
 struct TicketAnalyser: Analyser {
+    // The current observation's baseline is within 1% of the previous observation's baseline, it must belong to the current value.
+    let observationBaselineThreshold = 0.01
+    
     func analyse(observations: [VNRecognizedTextObservation]) -> String {
-        return observations
-            .compactMap { $0.topCandidates(self.maxRecognitionCandidates).first?.string }
-            .joined(separator: "\n")
+        var rows: [String] = []
+        var currentRow: String = ""
+        var previousObservation: VNRecognizedTextObservation?
+        let sortedObservations = observations.sorted(by: { lhs, rhs in
+            lhs.boundingBox.minY > rhs.boundingBox.minY
+        })
+        
+        for observation in sortedObservations {
+            guard let candidate = observation.topCandidates(maxRecognitionCandidates).first,
+                  (candidate.string.isUppercased()
+                   || candidate.string.contains("kg")) else { continue }
+            
+            if let previous = previousObservation {
+               if abs(observation.boundingBox.minY - previous.boundingBox.minY) < observationBaselineThreshold {
+                   currentRow += "\t\(candidate.string)"
+                   
+               } else {
+                   rows.append(currentRow)
+                   currentRow = ""
+               }
+            } 
+            
+            if currentRow.isEmpty {
+                currentRow = candidate.string
+            }
+            
+            previousObservation = observation
+        }
+        
+        return rows.joined(separator: "\n")
     }
 }
