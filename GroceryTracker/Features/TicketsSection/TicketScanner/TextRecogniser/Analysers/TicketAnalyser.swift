89dcab136d4446c85e2e641be94c0907cfa5ad81
Victor Barrios <victor.barrios@bestsecret.com>
2021-12-09 08:27:01 +0100

    Make the algorithm to consider the horizontal order for each entry

--- GroceryTracker/Features/TicketsSection/TicketScanner/TextRecogniser/Analysers/TicketAnalyser.swift
+++ GroceryTracker/Features/TicketsSection/TicketScanner/TextRecogniser/Analysers/TicketAnalyser.swift
@@ -2,13 +2,18 @@ import Foundation
 import VisionKit
 import Vision
 
+private struct TextItem {
+    let text: String
+    let minX: CGFloat
+}
+
 struct TicketAnalyser: Analyser {
     // The current observation's baseline is within 1% of the previous observation's baseline, it must belong to the current value.
     let observationBaselineThreshold = 0.01
     
     func analyse(observations: [VNRecognizedTextObservation]) -> String {
-        var rows: [String] = []
-        var currentRow: String = ""
+        var entryRows: [String] = []
+        var currentEntry: [TextItem] = []
         var previousObservation: VNRecognizedTextObservation?
         let sortedObservations = observations.sorted(by: { lhs, rhs in
             lhs.boundingBox.minY > rhs.boundingBox.minY
@@ -19,23 +24,30 @@ struct TicketAnalyser: Analyser {
                   (candidate.string.isUppercased()
                    || candidate.string.contains("kg")) else { continue }
             
+            let textItem = TextItem(text: candidate.string, minX: observation.boundingBox.minX)
+            
             if let previous = previousObservation {
                if abs(observation.boundingBox.minY - previous.boundingBox.minY) < observationBaselineThreshold {
-                   currentRow += "\t\(candidate.string)"
+                   if let index: Int = currentEntry.firstIndex(where: { $0.minX > textItem.minX }) {
+                       currentEntry.insert(textItem, at: index)
+                   } else {
+                       currentEntry.append(textItem)
+                   }
                    
                } else {
-                   rows.append(currentRow)
-                   currentRow = ""
+                   let row = currentEntry.map { $0.text }.joined(separator: "\t")
+                   entryRows.append(row)
+                   currentEntry.removeAll()
                }
             } 
             
-            if currentRow.isEmpty {
-                currentRow = candidate.string
+            if currentEntry.isEmpty {
+                currentEntry.append(textItem)
             }
             
             previousObservation = observation
         }
         
-        return rows.joined(separator: "\n")
+        return entryRows.joined(separator: "\n")
     }
 }
