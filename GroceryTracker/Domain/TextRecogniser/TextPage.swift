2b88de081b44e1ce9620ee3db92214e54231d26d
Victor Barrios <victor.barrios@bestsecret.com>
2022-10-20 23:39:11 +0200

    Fix 'row' can lead to 'index out of bounds' exception

--- GroceryTracker/Domain/TextRecogniser/TextPage.swift
+++ GroceryTracker/Domain/TextRecogniser/TextPage.swift
@@ -14,10 +14,16 @@ class TextPage: Identifiable {
     private(set) var rows: [Row] = []
     
     func addRow(_ row: [String], distribution: ColumnsDistribution) {
-        let newRow = Row(name: row[distribution.nameColumn.position],
-                         units: row[distribution.unitsColumn.position],
-                         singlePrice: row[distribution.singlePriceColumn.position],
-                         totalPrice: row[distribution.totalPriceColumn.position])
+        let newRow = Row(name: extractValue(from: row, position: distribution.nameColumn.position) ?? "",
+                         units: extractValue(from: row, position: distribution.unitsColumn.position) ?? "",
+                         singlePrice: extractValue(from: row, position: distribution.singlePriceColumn.position) ?? "",
+                         totalPrice: extractValue(from: row, position: distribution.totalPriceColumn.position) ?? "")
         rows.append(newRow)
+    }
+    
+    private func extractValue(from row: [String], position: Int) -> String? {
+        guard position < row.count else { return nil }
+        
+        return row[position]
     }
 }
