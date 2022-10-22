bfd854caf805d29dc30a94b106411436a12feebd
Victor Barrios <victor.barrios@bestsecret.com>
2022-10-22 21:56:18 +0200

    Remove commented code

--- GroceryTracker/Features/TicketsSection/Scanner/ScannerViewModel.swift
+++ GroceryTracker/Features/TicketsSection/Scanner/ScannerViewModel.swift
@@ -2,15 +2,10 @@ import Foundation
 import SwiftUI
 
 struct ScannerViewModel {
-//    @Binding var scannedTicketModel: ScannedTicketModel?
     let textRecogniser: TextRecogniser
     
     func recogniseText(from images: [CGImage]) -> ScannedTicketModel {
         let textPages = textRecogniser.text(from: images)
         return ScannedTicketModel(id: UUID(), pages: textPages)
-        
-//        DispatchQueue.main.async {
-//            self.scannedTicketModel = ScannedTicketModel(id: UUID(), pages: textPages)
-//        }
     }
 }
